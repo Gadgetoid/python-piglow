f7f0490adc6e3a1cc7100cf21877d4e43ad0d8e2
Phil Howard <Phil@gadgetoid.com>
2014-11-06 10:58:29 +0000

    Updated for newer sn3218

--- piglow.py
+++ piglow.py
@@ -1,92 +1,117 @@
-from sn3218 import sn3218
+import sn3218
 
-class PiGlow(sn3218):
+sn3218.enable()
+sn3218.enable_leds(0b111111111111111111)
 
-        legs = [
-                [ 6, 7, 8, 5, 4, 9 ],
-                [ 17, 16, 15, 13, 11, 10 ],
-                [ 0, 1, 2, 3, 14, 12 ]
-        ]
+legs = [
+  [ 6, 7, 8, 5, 4, 9 ],
+  [ 17, 16, 15, 13, 11, 10 ],
+  [ 0, 1, 2, 3, 14, 12 ]
+]
 
-	colours = {
-		"red" : 0,
-		"orange" : 1,
-		"yellow" : 2,
-		"green" : 3,
-		"blue" : 4,
-		"white" : 5
-	}
+values = [0] * 18
 
-        def __init__(self, i2c_bus=1):
-                sn3218.__init__(self, i2c_bus)
-		setattr(self, 'white',  lambda v: self.ring(5,v))
-		setattr(self, 'blue',   lambda v: self.ring(4,v))
-                setattr(self, 'green',  lambda v: self.ring(3,v))
-                setattr(self, 'yellow', lambda v: self.ring(2,v))
-                setattr(self, 'orange', lambda v: self.ring(1,v))
-                setattr(self, 'red',    lambda v: self.ring(0,v))
+colours = {
+  "red" : 0,
+  "orange" : 1,
+  "yellow" : 2,
+  "green" : 3,
+  "blue" : 4,
+  "white" : 5
+}
 
-		setattr(self, 'led1', lambda v: self.set(0,v))
-                setattr(self, 'led2', lambda v: self.set(1,v))
-                setattr(self, 'led3', lambda v: self.set(2,v))
-                setattr(self, 'led4', lambda v: self.set(3,v))
-                setattr(self, 'led5', lambda v: self.set(4,v))
-                setattr(self, 'led6', lambda v: self.set(5,v))
-                setattr(self, 'led7', lambda v: self.set(6,v))
-                setattr(self, 'led8', lambda v: self.set(7,v))
-                setattr(self, 'led9', lambda v: self.set(8,v))
-                setattr(self, 'led10', lambda v: self.set(9,v))
-                setattr(self, 'led11', lambda v: self.set(10,v))
-                setattr(self, 'led12', lambda v: self.set(11,v))
-                setattr(self, 'led13', lambda v: self.set(12,v))
-                setattr(self, 'led14', lambda v: self.set(13,v))
-                setattr(self, 'led15', lambda v: self.set(14,v))
-                setattr(self, 'led16', lambda v: self.set(15,v))
-                setattr(self, 'led17', lambda v: self.set(16,v))
-                setattr(self, 'led18', lambda v: self.set(17,v))			
+white  = lambda v: ring(5,v)
+blue   = lambda v: ring(4,v)
+green  = lambda v: ring(3,v)
+yellow = lambda v: ring(2,v)
+orange = lambda v: ring(1,v)
+red    = lambda v: ring(0,v)
 
-	def set(self, led, value):
-		sn3218.set(self, led, value)
-		self.update()
+led1   = lambda v: set(0,v)
+led2   = lambda v: set(1,v)
+led3   = lambda v: set(2,v)
+led4   = lambda v: set(3,v)
+led5   = lambda v: set(4,v)
+led6   = lambda v: set(5,v)
+led7   = lambda v: set(6,v)
+led8   = lambda v: set(7,v)
+led9   = lambda v: set(8,v)
+led10  = lambda v: set(9,v)
+led11  = lambda v: set(10,v)
+led12  = lambda v: set(11,v)
+led13  = lambda v: set(12,v)
+led14  = lambda v: set(13,v)
+led15  = lambda v: set(14,v)
+led16  = lambda v: set(15,v)
+led17  = lambda v: set(16,v)
+led18  = lambda v: set(17,v)			
 
-        def ring(self, ring, intensity):
-                self.set([self.legs[0][ring], self.legs[1][ring], self.legs[2][ring]], intensity)
+def show():
+  sn3218.output(values)
 
-        def leg(self, leg, intensity):
-                self.set(self.legs[leg], intensity)
+def set(leds, value):
+  if isinstance(leds, list):
+    for led in leds:
+      if isinstance(value, list):
+        values[leds[led]] = value[led]
+      else:
+        values[led] = value
+  elif isinstance(leds, int):
+    values[leds] = value
+  else:
+    raise ValueError("Invalid LED/LEDs")
+
+def ring(ring, intensity):
+  set([legs[0][ring], legs[1][ring], legs[2][ring]], intensity)
+
+
+def leg_bar(leg, percentage):
+  # 1530 = 6 * 255
+  amount = int(1530.0 * percentage)
+  for led in reversed(legs[leg]):
+    if amount >= 255:
+      set(led, 255)
+    elif amount > 0:
+      set(led,amount)
+    else:
+      set(led,0)
+    amount = amount - 255
+
+def leg(leg, intensity):
+  set(legs[leg], intensity)
 	
-	def arm(self, arm, intensity):
-		self.set(self.legs[arm-1], intensity)
+def arm(arm, intensity):
+  set(legs[arm-1], intensity)
 
-	def arm1(self, intensity):
-		self.arm(1, intensity)
+def arm1(intensity):
+  arm(1, intensity)
 
-	def arm2(self, intensity):
-		self.arm(2, intensity)
+def arm2(intensity):
+  arm(2, intensity)
 
-	def arm3(self, intensity):
-		self.arm(3, intensity)
+def arm3(intensity):
+  arm(3, intensity)
 
-	def led(self, led, intensity):
-		self.set(led-1, intensity)
+def led(led, intensity):
+  set(led-1, intensity)
 
-        def single(self, leg, ring, intensity):
-                self.set(self.legs[leg][ring], intensity)
+def single(leg, ring, intensity):
+  set(legs[leg][ring], intensity)
 
-	def colour(self, colour, intensity):
-		if not isinstance(colour, int):
-			if colour in self.colours:
-				colour = self.colours[colour]
-				self.ring(colour, intensity)
-				return True
-			else:
-				raise ValueError("Invalid Colour")
-				return False
-		self.ring(colour-1, intensity)
-		return True
+def colour(colour, intensity):
+  if not isinstance(colour, int):
+    if colour in colours:
+      colour = colours[colour]
+      ring(colour, intensity)
+      return True
+    else:
+      raise ValueError("Invalid Colour")
+      return False
+  ring(colour-1, intensity)
+  return True
 	
-	def all(self, value):
-		self.set(0, [value]*18)
+def all(value):
+  set(0, [value]*18)
 
-        def clear(self):
-                self.set(0, [0]*18)
+def clear():
+  set(0, [0]*18)
